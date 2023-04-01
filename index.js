4e2648c4220a732a66393722aceb6c025a3f34a8
none <none>
2023-04-01 21:17:35 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("Hello CodeSandbox");
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+app.use("/static", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect("static/chat-adv-client.html");
+});
+
+let users = [];
+
+io.on("connection", (socket) => {
+  socket.on("join", (msg) => {
+    users.push({
+      id: Math.floor(Math.random() * (70 - 1) + 1),
+      username: msg,
+    });
+    io.emit("users", users);
+    io.emit("event", `${msg} has joined!`);
+  });
+
+  socket.on("part", (msg) => {
+    users.forEach((value, key) => {
+      if (value.username == msg) {
+        users.splice(key, 1);
+      }
+    });
+    io.emit("users", users);
+    io.emit("event", `${msg} has left!`);
+  });
+
+  socket.on("message", (msg) => {
+    socket.broadcast.emit("message", msg);
+  });
+});
+
+httpServer.listen(3000);
--- public/chat-adv-client.js
+++ public/chat-adv-client.js
@@ -1,15 +1,69 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // get user name and then tell the server
-  let username = prompt("What's your username?");
+  let usersList = document.querySelector("#users ul");
+  let messagesList = document.querySelector("#messages ul");
+  let entry = document.querySelector("#entry");
 
-  /* This user is sending a new chat message */
+  let socket = io();
+  let username = prompt("What's your username?");
+  socket.emit("join", username);
+
+  socket.on("users", (msg) => {
+    usersList.innerHTML = "";
+    msg.forEach((value) => {
+      usersList.innerHTML += `
+      <li>
+        <img src="https://randomuser.me/api/portraits/med/men/${value.id}.jpg" alt="avatar" />
+        <div class="name">
+            <p>${value.username}</p>
+            <p>online</p>
+        </div>
+      </li>
+      `;
+    });
+  });
+
+  socket.on("event", (msg) => {
+    messagesList.innerHTML += `
+    <li class="message-user">
+      <p class="message-text">${msg}</p>
+    </li>
+    `;
+  });
+
+  socket.on("message", (msg) => {
+    messagesList.innerHTML += `
+    <li class="message-received">
+      <p class="message-data">
+        ${msg.username} <span>Today</span>
+      </p>
+      <p class="message-text">${msg.entry}</p>
+    </li>
+    `;
+    scrollDown();
+  });
+
   document.querySelector("#chatForm").addEventListener("submit", (e) => {
     e.preventDefault();
-    const entry = document.querySelector("#entry");
+    socket.emit("message", { username, entry: entry.value });
+    messagesList.innerHTML += `
+    <li class="message-sent">
+      <p class="message-data">
+        ${username} <span>Today</span>
+      </p>
+      <p class="message-text">${entry.value}</p>
+    </li>
+    `;
+    entry.value = "";
+    scrollDown();
   });
 
-  /* User has clicked the leave button */
   document.querySelector("#leave").addEventListener("click", (e) => {
     e.preventDefault();
+    socket.emit("part", username);
   });
+
+  function scrollDown() {
+    let messages = document.querySelector(".messages-body");
+    messages.scrollTop = messages.scrollHeight;
+  }
 });
